c99f136aa7c5df3bbcb916d71e9f8a4986f6ce62
AlejandroBalladares <44703962+AlejandroBalladares@users.noreply.github.com>
2018-11-14 19:31:12 -0300

    Implemento su constructor y el avanzarTurno
    Utilice el metodo avanzarTurno que sabe responder Plaza pero deberian conocerlo tanto edificios como unidades. Perteneceria a la interfaz "Colocable"

--- src/modelo/Civilizacion.java
+++ src/modelo/Civilizacion.java
@@ -1,11 +1,45 @@
+
 package modelo;
+
+import unidades.colocable;
+import unidades.unidad;
+import edificios.PlazaCentral;
+import modelo.Oro;
+import modelo.mapa.Casillero;
 
 public class Civilizacion {
 	
-	Oro oro = new Oro(100);
+	Oro oro;
+	ArrayList<Unidad> unidades;
+	ArrayList<Edificio> edificios;
+	limitePoblacion = 50;
+	int aldeanosIniciales = 3;
 	
-	public Civilizacion() {
+	/*
+	public Civilizacion(int PosCastilloX, int PosCastilloY) {
+		oro = new Oro(100);
+		unidades = new ArrayList<>();
+		PlazaCentral plaza = new PlazaCentral(oro);
+		plaza.colocarseEn(mapa, 1, 1);
 		
+		for(int i=0; i<aldeanosIniciales; i++){
+			Unidad unidad = plaza.crearAldeanoDesdePlaza();
+			Casillero casillero = plaza.casilleroAlrededorDisponible(); //falta implementar
+			casillero.colocar(Colocable colocable);
+			unidades.add(unidad);
+		}
 	}
-
+	
+	public avanzarTurnoCivilizacion(){
+		for(Edificio edificio: edificios){
+			edificio.avanzarTurno();
+		}
+		for(Unidad unidad: unidades){
+			unidad.avanzarTurno();
+		}
+	}
 }
+	*/
+
+	
+
