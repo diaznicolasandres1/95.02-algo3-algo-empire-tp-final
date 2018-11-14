726fe7241bf415eca6f92fc8229a9201f3e5e0ce
Facundo Mastricchio <facundomastricchio@gmail.com>
2018-11-14 14:28:24 -0300

    Agrego movimiento de unidades

--- src/modelo/mapa/Mapa.java
+++ src/modelo/mapa/Mapa.java
@@ -1,6 +1,7 @@
 package modelo.mapa;
 
 import edificios.Edificio;
+import modelo.Posicion;
 import unidades.Unidad;
 
 import java.util.ArrayList;
@@ -24,6 +25,32 @@ public class Mapa {
         this.altura = altura;
         this.agregarCasilleros();
         this.agregarFilas();
+    }
+
+    public void colocarUnidad(Unidad unidad, int fila, int columna) {
+
+        Casillero casillero = this.buscarCasillero(fila, columna);
+        Posicion posicion = new Posicion(columna, fila);
+        casillero.colocarUnidad(unidad);
+        unidad.setPosicion(posicion);
+    }
+
+    public void colocarEdificio(Edificio edificio, int tamanioEdificio, int fila, int columna) {
+
+        ArrayList<Casillero> terreno = this.buscarCasillerosParaEdificio(tamanioEdificio, fila, columna);
+        for (Casillero casillero : terreno) {
+            casillero.colocarEdificio(edificio);
+        }
+    }
+
+    public void moverUnidadDesdeHasta(Unidad unidad, int filaOrigen, int columnaOrigen, int filaDestino, int columnaDestino) {
+
+        Casillero casilleroOrigen = this.buscarCasillero(filaOrigen, columnaOrigen);
+        Casillero casilleroDestino = this.buscarCasillero(filaDestino, columnaDestino);
+        Posicion posicion = new Posicion(columnaDestino, filaOrigen);
+        unidad.moverHacia(posicion);
+        casilleroDestino.colocarUnidad(unidad);
+        casilleroOrigen.desocupar();
     }
 
     private void agregarCasilleros() {
@@ -69,19 +96,5 @@ public class Mapa {
     private boolean esTamanioValido(int base, int altura) {
 
         return (base >= 12 && altura >= 12);
-    }
-
-    public void colocarUnidad(Unidad unidad, int fila, int columna) {
-
-        Casillero casillero = this.buscarCasillero(fila, columna);
-        casillero.colocarUnidad(unidad);
-    }
-
-    public void colocarEdificio(Edificio edificio, int tamanioEdificio, int fila, int columna) {
-
-        ArrayList<Casillero> terreno = this.buscarCasillerosParaEdificio(tamanioEdificio, fila, columna);
-        for (Casillero casillero : terreno) {
-            casillero.colocarEdificio(edificio);
-        }
     }
 }
