49b0f3ac0d5c5431007cc864f12e7c61d3449731
Facundo Mastricchio <facundomastricchio@gmail.com>
2018-11-14 14:29:00 -0300

    Agrego metodos para mover unidad

--- src/unidades/Unidad.java
+++ src/unidades/Unidad.java
@@ -1,9 +1,13 @@
 package unidades;
 
+import modelo.Posicion;
+
 public abstract class Unidad {
+
 	protected int vida;
 	protected int costo;
-	
+    protected Posicion posicion;
+    protected int rangoMovimiento = 1;
 	
 	public int getVida() {
 		return vida;
@@ -15,7 +19,12 @@ public abstract class Unidad {
         }
         vida -= danio;
     }
-	
 
-	
+    public void moverHacia(Posicion posicion) {
+        this.posicion.moverUnidadHacia(this, posicion, rangoMovimiento);
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
 }
