4df71dbc9e0af04b2a49cb7a93220ed159332946
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2018-11-13 21:27:17 -0300

    Agrego Posicion otra vez

---
+++ src/modelo/Posicion.java
@@ -0,0 +1,28 @@
+package modelo;
+
+public class Posicion {
+	private int posX;
+	private int posY;
+	
+	public Posicion(int newPosX, int newPosY) {
+		this.posX = newPosX;
+		this.posY = newPosY;
+	}
+	
+	public int getPosX() {
+		return this.posX;
+	}
+	
+	public int getPosY() {
+		return this.posY;
+	}
+
+	public boolean dentroRango(Posicion otraPosicion, int rango) {
+		int rangoX = this.posX - otraPosicion.getPosX();
+		int rangoY = this.posY - otraPosicion.getPosY();
+		boolean condicion1 = (rangoX <= rango) && (rangoX >= -rango);
+		boolean condicion2 = (rangoY <= rango) && (rangoY >= -rango);
+		return condicion1 && condicion2;
+	}
+}
+
