57b138a05760e4e5995cd2a57f79ba329c22f859
Nicolas Diaz <diaz.nicolasandres@gmail.com>
2018-11-14 13:44:44 -0300

    Crear Arma asedio cuesta oro

--- test/modelo/ArmaAsedioTest.java
+++ test/modelo/ArmaAsedioTest.java
@@ -14,13 +14,15 @@ public class ArmaAsedioTest {
 
 	@Test
 	public void test01CreacionDeArmaAsedio() {
-		ArmaDeAsedio arma = new ArmaDeAsedio();		
+		Oro oro = new Oro(500);
+		ArmaDeAsedio arma = new ArmaDeAsedio(oro);		
 		Assert.assertEquals(arma.getVida(),150);		
 	}
 	
 	@Test(expected = NoSePuedeMoverArmaAsedioMontadaException.class)
 	public void test02MoverArmaMontadaLanzaExcepcion() {
-		ArmaDeAsedio arma = new ArmaDeAsedio();	
+		Oro oro = new Oro(500);
+		ArmaDeAsedio arma = new ArmaDeAsedio(oro);	
 		arma.montarArma();
 		arma.moverse();
 		
@@ -28,8 +30,23 @@ public class ArmaAsedioTest {
 	
 	@Test(expected = NoSePuedeAtacarArmaAsedioDesmontadaException.class)
 	public void test03AtacarConArmaDesmontada() {
-		ArmaDeAsedio arma = new ArmaDeAsedio();			
+		Oro oro = new Oro(500);
+		ArmaDeAsedio arma = new ArmaDeAsedio(oro);			
 		arma.atacar();		
+	}
+	
+	@Test 
+	public void test04CrearArmaSedioCuestaOro() {
+		Oro oro = new Oro(500);
+		ArmaDeAsedio arma = new ArmaDeAsedio(oro);
+		Assert.assertEquals(oro.getOro(), 300);
+	}
+	
+	@Test(expected = NoTenesOroSuficienteException.class)
+	public void test05CrearArmaSinOroDisponible() {
+		Oro oro = new Oro(5);
+		ArmaDeAsedio arma = new ArmaDeAsedio(oro);
+		
 	}
 	
 }
