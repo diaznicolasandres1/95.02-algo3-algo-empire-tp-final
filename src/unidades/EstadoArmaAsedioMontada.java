95e5c863de1923d75055dc1c1906f8021fc241ea
Facundo Mastricchio <facundomastricchio@gmail.com>
2018-11-14 22:27:14 -0300

    Cambios menores

--- src/unidades/EstadoArmaAsedioMontada.java
+++ src/unidades/EstadoArmaAsedioMontada.java
@@ -1,18 +1,16 @@
 package unidades;
 
+import modelo.Posicion;
+
 public class EstadoArmaAsedioMontada implements EstadoArmaAsedio{
-	
+
+	@Override
 	public void atacar() {
-   //como esta montada se puede atacar, se implementa para la prox entrega
-	}
-	
-	
-	
-	
-	public void moverse() throws NoSePuedeMoverArmaAsedioMontadaException  {	
-		
-		throw new NoSePuedeMoverArmaAsedioMontadaException();
-		
+		//TODO proxima entrega
 	}
 
+	@Override
+	public void moverUnidadDesdeHacia(Unidad unidad, Posicion origen, Posicion destino, int rangoMovimiento) throws NoSePuedeMoverArmaAsedioMontadaException {
+		throw new NoSePuedeMoverArmaAsedioMontadaException();
+	}
 }
