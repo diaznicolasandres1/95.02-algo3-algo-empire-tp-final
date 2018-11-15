5e1bfb8bc627cd04c466f409f53d0aeddd3d7064
Facundo Mastricchio <facundomastricchio@gmail.com>
2018-11-14 22:24:26 -0300

    Agrego tests referidos al movimiento del Arquero

--- test/modelo/ArqueroTest.java
+++ test/modelo/ArqueroTest.java
@@ -1,36 +1,47 @@
 package modelo;
 
-import org.junit.Test;
-
 import junit.framework.Assert;
+import org.junit.Test;
 import unidades.Arquero;
-import unidades.Espadachin;
 
 public class ArqueroTest {
 	
 	@Test
 	public void test01CreacionDeArquero() {
-		Oro oro = new Oro(300);
-		Arquero arquero = new Arquero(oro);		
-		Assert.assertEquals(arquero.getVida(), 75);
-		
-		}
 
+		Oro oro = new Oro(300);
+        Arquero arquero = new Arquero(oro);
+
+		Assert.assertEquals(arquero.getVida(), 75);
+    }
 	
 	@Test 
 	public void test02CreacionDeArqueroCuestaOro() {
+
 		Oro oro = new Oro(300);
-		Arquero arquero = new Arquero(oro);	
+        Arquero arquero = new Arquero(oro);
+
 		Assert.assertEquals(oro.getOro(), 225);
-		
 	}
 
 	@Test(expected = NoTenesOroSuficienteException.class)
-	public void test09CrearEspadachinConOroInsuficiente() {
+    public void test03CrearArqueroConOroInsuficiente() {
+
 		Oro oro = new Oro(5);
-		Espadachin espadachin = new Espadachin(oro);	
-	}
-	
-	
-	
+
+        Arquero arquero = new Arquero(oro);
+    }
+
+    @Test(expected = PosicionFueraDeRangoException.class)
+    public void test04arqueroSeMueveFueraDeRangoYLanzaExcepcion() {
+
+        Oro oro = new Oro(300);
+        Arquero arquero = new Arquero(oro);
+        Posicion unaPosicion = new Posicion(5, 5);
+        Posicion otraPosicion = new Posicion(10, 10);
+
+        arquero.setPosicion(unaPosicion);
+
+        arquero.moverHacia(otraPosicion);
+    }
 }
