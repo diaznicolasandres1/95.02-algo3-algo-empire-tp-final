2c5eaa1d3198a2af16a8fd70bdf0a1939e709d49
Facundo Mastricchio <facundomastricchio@gmail.com>
2018-11-14 22:23:46 -0300

    Agrego tests referidos al movimiento del ArmaDeAsedio

--- test/modelo/ArmaAsedioTest.java
+++ test/modelo/ArmaAsedioTest.java
@@ -1,11 +1,7 @@
 package modelo;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 import junit.framework.Assert;
-import unidades.Aldeano;
+import org.junit.Test;
 import unidades.ArmaDeAsedio;
 import unidades.NoSePuedeAtacarArmaAsedioDesmontadaException;
 import unidades.NoSePuedeMoverArmaAsedioMontadaException;
@@ -14,39 +10,63 @@ public class ArmaAsedioTest {
 
 	@Test
 	public void test01CreacionDeArmaAsedio() {
+
 		Oro oro = new Oro(500);
-		ArmaDeAsedio arma = new ArmaDeAsedio(oro);		
-		Assert.assertEquals(arma.getVida(),150);		
+		ArmaDeAsedio arma = new ArmaDeAsedio(oro);
+
+		Assert.assertEquals(arma.getVida(), 150);
+
 	}
 	
 	@Test(expected = NoSePuedeMoverArmaAsedioMontadaException.class)
 	public void test02MoverArmaMontadaLanzaExcepcion() {
+
 		Oro oro = new Oro(500);
-		ArmaDeAsedio arma = new ArmaDeAsedio(oro);	
+		ArmaDeAsedio arma = new ArmaDeAsedio(oro);
+		Posicion posicion = new Posicion(9, 9);
+
 		arma.montarArma();
-		arma.moverse();
+
+		arma.moverHacia(posicion);
 		
 	}
 	
 	@Test(expected = NoSePuedeAtacarArmaAsedioDesmontadaException.class)
 	public void test03AtacarConArmaDesmontada() {
-		Oro oro = new Oro(500);
-		ArmaDeAsedio arma = new ArmaDeAsedio(oro);			
-		arma.atacar();		
-	}
-	
-	@Test 
-	public void test04CrearArmaSedioCuestaOro() {
+
 		Oro oro = new Oro(500);
 		ArmaDeAsedio arma = new ArmaDeAsedio(oro);
+
+		arma.atacar();
+	}
+	
+	@Test
+	public void test04CrearArmaDeAsedioCuestaOro() {
+
+		Oro oro = new Oro(500);
+		ArmaDeAsedio arma = new ArmaDeAsedio(oro);
+
 		Assert.assertEquals(oro.getOro(), 300);
 	}
 	
 	@Test(expected = NoTenesOroSuficienteException.class)
 	public void test05CrearArmaSinOroDisponible() {
 		Oro oro = new Oro(5);
+
 		ArmaDeAsedio arma = new ArmaDeAsedio(oro);
-		
+	}
+
+	@Test(expected = PosicionFueraDeRangoException.class)
+	public void test06armaDeAsedioSeMueveFueraDeRangoLanzaExcepcion() {
+
+		Oro oro = new Oro(500);
+		ArmaDeAsedio arma = new ArmaDeAsedio(oro);
+		Posicion unaPosicion = new Posicion(5, 5);
+		Posicion otraPosicion = new Posicion(9, 9);
+
+		arma.setPosicion(unaPosicion);
+
+		arma.moverHacia(otraPosicion);
 	}
 	
 }
