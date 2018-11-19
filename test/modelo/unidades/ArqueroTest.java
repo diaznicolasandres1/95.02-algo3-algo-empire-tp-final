2a9bf01276d7a14a748b92b95b69ab3e5c116953
Facundo Mastricchio <facundomastricchio@gmail.com>
2018-11-18 22:49:17 -0300

    Agrego tests y muevo de package

--- test/modelo/unidades/ArqueroTest.java
+++ test/modelo/unidades/ArqueroTest.java
@@ -1,10 +1,13 @@
-package modelo;
+package modelo.unidades;
 
 import junit.framework.Assert;
+import modelo.NoTenesOroSuficienteException;
+import modelo.Oro;
+import modelo.Posicion;
+import modelo.PosicionFueraDeRangoException;
 import modelo.mapa.CasilleroOcupadoException;
 import modelo.mapa.Mapa;
 import org.junit.Test;
-import unidades.Arquero;
 
 public class ArqueroTest {
 	
@@ -55,10 +58,10 @@ public class ArqueroTest {
         Arquero unArquero = new Arquero(oro);
         Arquero otroArquero = new Arquero(oro);
 
-        mapa.colocarUnidad(unArquero, 10, 10);
+        unArquero.colocarseEn(mapa, 10, 10);
         mapa.moverUnidadDesdeHasta(unArquero, 10, 10, 9, 10);
 
-        mapa.colocarUnidad(otroArquero, 9, 10);
+        otroArquero.colocarseEn(mapa, 9, 10);
     }
 
     @Test(expected = CasilleroOcupadoException.class)
@@ -69,10 +72,10 @@ public class ArqueroTest {
         Arquero unArquero = new Arquero(oro);
         Arquero otroArquero = new Arquero(oro);
 
-        mapa.colocarUnidad(unArquero, 10, 10);
+        unArquero.colocarseEn(mapa, 10, 10);
         mapa.moverUnidadDesdeHasta(unArquero, 10, 10, 11, 10);
 
-        mapa.colocarUnidad(otroArquero, 11, 10);
+        otroArquero.colocarseEn(mapa, 11, 10);
     }
 
     @Test(expected = CasilleroOcupadoException.class)
@@ -83,10 +86,10 @@ public class ArqueroTest {
         Arquero unArquero = new Arquero(oro);
         Arquero otroArquero = new Arquero(oro);
 
-        mapa.colocarUnidad(unArquero, 10, 10);
+        unArquero.colocarseEn(mapa, 10, 10);
         mapa.moverUnidadDesdeHasta(unArquero, 10, 10, 10, 11);
 
-        mapa.colocarUnidad(otroArquero, 10, 11);
+        otroArquero.colocarseEn(mapa, 10, 11);
     }
 
     @Test(expected = CasilleroOcupadoException.class)
@@ -97,10 +100,10 @@ public class ArqueroTest {
         Arquero unArquero = new Arquero(oro);
         Arquero otroArquero = new Arquero(oro);
 
-        mapa.colocarUnidad(unArquero, 10, 10);
+        unArquero.colocarseEn(mapa, 10, 10);
         mapa.moverUnidadDesdeHasta(unArquero, 10, 10, 10, 9);
 
-        mapa.colocarUnidad(otroArquero, 10, 9);
+        otroArquero.colocarseEn(mapa, 10, 9);
     }
 
     @Test(expected = CasilleroOcupadoException.class)
@@ -111,10 +114,10 @@ public class ArqueroTest {
         Arquero unArquero = new Arquero(oro);
         Arquero otroArquero = new Arquero(oro);
 
-        mapa.colocarUnidad(unArquero, 10, 10);
+        unArquero.colocarseEn(mapa, 10, 10);
         mapa.moverUnidadDesdeHasta(unArquero, 10, 10, 9, 11);
 
-        mapa.colocarUnidad(otroArquero, 9, 11);
+        otroArquero.colocarseEn(mapa, 9, 11);
     }
 
     @Test(expected = CasilleroOcupadoException.class)
@@ -125,10 +128,10 @@ public class ArqueroTest {
         Arquero unArquero = new Arquero(oro);
         Arquero otroArquero = new Arquero(oro);
 
-        mapa.colocarUnidad(unArquero, 10, 10);
+        unArquero.colocarseEn(mapa, 10, 10);
         mapa.moverUnidadDesdeHasta(unArquero, 10, 10, 11, 11);
 
-        mapa.colocarUnidad(otroArquero, 11, 11);
+        otroArquero.colocarseEn(mapa, 11, 11);
     }
 
     @Test(expected = CasilleroOcupadoException.class)
@@ -139,10 +142,10 @@ public class ArqueroTest {
         Arquero unArquero = new Arquero(oro);
         Arquero otroArquero = new Arquero(oro);
 
-        mapa.colocarUnidad(unArquero, 10, 10);
+        unArquero.colocarseEn(mapa, 10, 10);
         mapa.moverUnidadDesdeHasta(unArquero, 10, 10, 11, 9);
 
-        mapa.colocarUnidad(otroArquero, 11, 9);
+        otroArquero.colocarseEn(mapa, 11, 9);
     }
 
     @Test(expected = CasilleroOcupadoException.class)
@@ -153,9 +156,53 @@ public class ArqueroTest {
         Arquero unArquero = new Arquero(oro);
         Arquero otroArquero = new Arquero(oro);
 
-        mapa.colocarUnidad(unArquero, 10, 10);
+        unArquero.colocarseEn(mapa, 10, 10);
         mapa.moverUnidadDesdeHasta(unArquero, 10, 10, 9, 9);
 
-        mapa.colocarUnidad(otroArquero, 9, 9);
+        otroArquero.colocarseEn(mapa, 9, 9);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void test13arqueroIntentaColocarseFueraDelRangoPositivoDelMapaLanzaExcepcion() {
+
+        Mapa mapa = new Mapa(20, 20);
+        Oro oro = new Oro(200);
+        Arquero arquero = new Arquero(oro);
+
+        arquero.colocarseEn(mapa, 100, 100);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void test14arqueroIntentaColocarseFueraDelRangoNegativoDelMapaLanzaExcepcion() {
+
+        Mapa mapa = new Mapa(20, 20);
+        Oro oro = new Oro(200);
+        Arquero arquero = new Arquero(oro);
+
+        arquero.colocarseEn(mapa, -1000, -1000);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void test15arqueroIntentaColocarseEnLimiteDelRangoNuloDelMapaLanzaExcepcion() {
+
+        Mapa mapa = new Mapa(20, 20);
+        Oro oro = new Oro(200);
+        Arquero arquero = new Arquero(oro);
+
+        arquero.colocarseEn(mapa, 0, 0);
+    }
+
+    @Test(expected = CasilleroOcupadoException.class)
+    public void test16arqueroSeColocaEnMapaYSeDescolocaYSeColocanDosUnidadesEnElMismoLugarLanzaExcepcion() {
+
+        Mapa mapa = new Mapa(20, 20);
+        Oro oro = new Oro(500);
+        Arquero arquero = new Arquero(oro);
+
+        arquero.colocarseEn(mapa, 10, 10);
+        arquero.descolocarseDe(mapa);
+        mapa.colocarUnidad(new Arquero(oro), 10, 10);
+
+        mapa.colocarUnidad(new Arquero(oro), 10, 10);
     }
 }
