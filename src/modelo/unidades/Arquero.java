8f378576cfc7868cc855ec8493843b44cb3f0f60
Nicolas Diaz <diaz.nicolasandres@gmail.com>
2018-11-20 17:45:21 -0300

    Arquero ataca Edificio y unidad

--- src/modelo/unidades/Arquero.java
+++ src/modelo/unidades/Arquero.java
@@ -1,11 +1,25 @@
 package modelo.unidades;
 
+import modelo.Ataque;
 import modelo.Oro;
+import modelo.edificios.Edificio;
 
-public class Arquero extends Unidad {
+public class Arquero extends Unidad implements Ataque {
 	
 	public Arquero(Oro oro) {
 		vida = 75;
 		oro.restarOro(75);
+	}
+
+	@Override
+	public void atacar(Edificio edificio) {
+		edificio.recibirDanioConValor(10);
+		
+	}
+
+	@Override
+	public void atacar(Unidad unidad) {
+		unidad.recibirDanio(10);
+		
 	}
 }
