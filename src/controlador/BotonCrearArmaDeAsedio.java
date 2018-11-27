b110837efda8420f52d10c0154b0a7edabf8558e
Nicolas Diaz <diaz.nicolasandres@gmail.com>
2018-11-27 20:42:05 -0300

    boton arma de asedio

--- src/controlador/BotonCrearArmaDeAsedio.java
+++ src/controlador/BotonCrearArmaDeAsedio.java
@@ -3,28 +3,27 @@ package controlador;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
-import modelo.edificios.cuartel.Cuartel;
+import modelo.edificios.castillo.Castillo;
 import modelo.excepciones.OroInsuficienteException;
 import modelo.juego.Juego;
 
-public class BotonCrearEspadachin implements EventHandler<ActionEvent> {
+public class BotonCrearArmaDeAsedio  implements EventHandler<ActionEvent> {
 
     private Juego juego;
-    private Cuartel cuartel;
+    private Castillo castillo;
 
-    public BotonCrearEspadachin(Juego juego, Cuartel cuartel){
+    public BotonCrearArmaDeAsedio(Juego juego, Castillo castillo){
         this.juego = juego;
-        this.cuartel = cuartel;
+        this.castillo = castillo;
     }
-
     @Override
     public void handle(ActionEvent actionEvent) {
         try{
-            juego.crearEspadachin(this.cuartel);
+            juego.crearArmaDeAsedio(this.castillo);
         }catch(OroInsuficienteException e){
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Error al crear Espadachin");
-            alert.setContentText("No tienes oro suficiente para crear un Espadachin");
+            alert.setTitle("Error al crear arma de asedio");
+            alert.setContentText("No tienes oro suficiente para crear una arma de asedio");
             alert.show();
         }
 
