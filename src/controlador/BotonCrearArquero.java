357634fbb1259c41ba1dc6402f45da77a9b43e96
Nicolas Diaz <diaz.nicolasandres@gmail.com>
2018-11-27 20:38:28 -0300

    boton arquero

---
+++ src/controlador/BotonCrearArquero.java
@@ -0,0 +1,32 @@
+package controlador;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import modelo.edificios.cuartel.Cuartel;
+import modelo.edificios.plazacentral.PlazaCentral;
+import modelo.excepciones.OroInsuficienteException;
+import modelo.juego.Juego;
+
+public class BotonCrearArquero  implements EventHandler<ActionEvent> {
+    private Juego juego;
+    private Cuartel cuartel;
+
+    public BotonCrearArquero( Juego juego, Cuartel cuartel){
+        this.juego = juego;
+        this.cuartel = cuartel;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        try{
+            juego.crearArquero(cuartel);
+        }catch(OroInsuficienteException e){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error al crear arquero");
+            alert.setContentText("No tienes oro suficiente para crear un arquero");
+            alert.show();
+        }
+    }
+}
+
