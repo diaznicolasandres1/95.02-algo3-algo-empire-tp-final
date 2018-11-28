ffb15a24b5927da3f44f9b5bc1b5b79adb1dadf4
Nicolas Diaz <diaz.nicolasandres@gmail.com>
2018-11-27 23:14:19 -0300

    boton mover unidades

---
+++ src/controlador/BotonMoverUnidadHacia.java
@@ -0,0 +1,44 @@
+package controlador;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import modelo.excepciones.CasilleroOcupadoException;
+import modelo.excepciones.CoordenadasInvalidasException;
+import modelo.excepciones.EdificioSeleccionadoNoPerteneceAJugadorException;
+import modelo.excepciones.OroInsuficienteException;
+import modelo.juego.Juego;
+import modelo.unidades.Unidad;
+
+public class BotonMoverUnidadHacia   implements EventHandler<ActionEvent> {
+
+    private Juego juego;
+    private Unidad unidad;
+    private int fila;
+    private int columna;
+
+    public BotonMoverUnidadHacia(Juego juego, Unidad unidad,int fila, int columna){
+        this.juego = juego;
+        this.unidad = unidad;
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        try{
+            juego.moverUnidadHacia(unidad,fila,columna);
+        }catch(CasilleroOcupadoException e){
+            alert.setTitle("Error al mover unidad");
+            alert.setContentText("Ese casillero ya esta ocupado");
+            alert.show();
+        }catch (EdificioSeleccionadoNoPerteneceAJugadorException e){
+            alert.setTitle("Error al mover unidad");
+            alert.setContentText("No podes mover un edificio que no te pertenece");
+            alert.show();
+        }
+
+
+    }
+}
