2909fe72433accc6b716931f7768cc1493bd843d
Nicolas Diaz <diaz.nicolasandres@gmail.com>
2018-12-01 15:14:51 -0300

    Fondo de contenedor de juego

--- src/vista/ContenedorPrincipal.java
+++ src/vista/ContenedorPrincipal.java
@@ -16,10 +16,12 @@ import controlador.botonesdeedificios.BotonCrearEspadachinEventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import modelo.edificios.castillo.Castillo;
 import modelo.edificios.cuartel.Cuartel;
@@ -40,7 +42,7 @@ public class ContenedorPrincipal extends BorderPane {
     private String jugadorDos;
     private VBox izquierdo = new VBox();
     private VBox derecho = new VBox();
-    private VBox bottom = new VBox();
+    private HBox bottom = new HBox();
     public ArrayList<Boton> botones = new ArrayList<Boton>();
 
     public ContenedorPrincipal(String unJugador, String otroJugador) {
@@ -52,23 +54,30 @@ public class ContenedorPrincipal extends BorderPane {
         this.setCostados();
         this.crearBottom();
         this.cambiadorDeHandler =  new CambiadorDeHandler(juego,this,tablero);
+        Image fondo = new Image("file:src/vista/imagenes/fondoContenedor.jpg", 1600, 920, false, true);
+        BackgroundImage imagenFondo = new BackgroundImage(fondo, BackgroundRepeat.SPACE, BackgroundRepeat.SPACE, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        this.setBackground(new Background(imagenFondo));
     }
 
 
 
     private void setCostados(){
 
-        Label tituloIzq = new Label(this.jugadorUno);
-        Label tituloDer = new Label(this.jugadorDos);
+        Text tituloIzq = new Text(this.jugadorUno);
+        Text tituloDer = new Text(this.jugadorDos);
         BotonCambiarTurnoEventHandler cambiadorTurno = new BotonCambiarTurnoEventHandler(juego,this);
         Boton botonCambioTurno1 = new Boton("Cambiar Turno",cambiadorTurno);
         Boton botonCambioTurno2 = new Boton("Cambiar Turno",cambiadorTurno);
         botonCambioTurno2.setPadding(new Insets(15));
         botonCambioTurno1.setPadding(new Insets(15));
-        tituloDer.setPadding(new Insets(15));
-        tituloIzq.setPadding(new Insets(15));
+        //tituloDer.setPadding(new Insets(15));
+        //tituloIzq.setPadding(new Insets(15));
         tituloIzq.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
         tituloDer.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
+        tituloDer.setFill(Color.WHITE);
+        tituloIzq.setFill(Color.WHITE);
+
+
         izquierdo.setAlignment(Pos.TOP_LEFT);
         derecho.setAlignment(Pos.TOP_RIGHT);
         izquierdo.setSpacing(20);
@@ -83,7 +92,7 @@ public class ContenedorPrincipal extends BorderPane {
     }
 
     public void crearBottom(){
-        this.bottom = new VBox();
+        this.bottom = new HBox();
         setjugadorActual();
         Label comienzo = new Label("Clickea en una unidad o edificio");
         bottom.getChildren().add(comienzo);
@@ -117,7 +126,7 @@ public class ContenedorPrincipal extends BorderPane {
     }
 
     public void dibujarMetodosAldeano(Aldeano aldeano){
-        this.bottom = new VBox(); //Reinicio el vbox de bottom
+        this.bottom = new HBox(); //Reinicio el vbox de bottom
 
         BotonConstruirCuartelInicioEventHandler cuartelEventHandler = new BotonConstruirCuartelInicioEventHandler(juego,aldeano,this);
         Boton construirCuartel = new Boton("Construir Cuartel",cuartelEventHandler);
@@ -138,7 +147,7 @@ public class ContenedorPrincipal extends BorderPane {
     }
 
     public void dibujarMetodosCuartel(Cuartel cuartel){
-        this.bottom = new VBox(); //Reinicio el vbox de bottom
+        this.bottom = new HBox(); //Reinicio el vbox de bottom
 
         BotonCrearArqueroEventHandler arqueroHandler = new BotonCrearArqueroEventHandler(juego,cuartel,this);
         Boton crearArquero = new Boton("Crear arquero",arqueroHandler);
@@ -156,7 +165,7 @@ public class ContenedorPrincipal extends BorderPane {
     }
 
     public void dibujarMetodosArmaDeAsedio(ArmaDeAsedio armaDeAsedio){
-        this.bottom = new VBox(); //Reinicio el vbox de bottom
+        this.bottom = new HBox(); //Reinicio el vbox de bottom
 
         BotonMontarArmaEventHandler montarArmaEventHandler = new BotonMontarArmaEventHandler(juego,armaDeAsedio,this );
         Boton montarArma = new Boton("Montar arma de asedio",montarArmaEventHandler);
@@ -171,7 +180,7 @@ public class ContenedorPrincipal extends BorderPane {
     }
 
     public void dibujarMetodosPlazaCentral(PlazaCentral plaza){
-        this.bottom = new VBox(); //Reinicio el vbox de bottom
+        this.bottom = new HBox(); //Reinicio el vbox de bottom
         BotonCrearAldeanoEventHandler crearAldeanoEventHandler  = new BotonCrearAldeanoEventHandler(juego,plaza,this);
         Boton crearAldeano = new Boton("Crear Aldeano",crearAldeanoEventHandler);
         setjugadorActual();
@@ -181,7 +190,7 @@ public class ContenedorPrincipal extends BorderPane {
     }
 
     public void dibujarMetodoEspadachinOArquero(Atacante atacante){
-        this.bottom = new VBox(); //Reinicio el vbox de bottom
+        this.bottom = new HBox(); //Reinicio el vbox de bottom
         BotonAtacarInicioEventHandler atacarInicioEventHandler = new BotonAtacarInicioEventHandler(juego,atacante,this);
         Boton atacar = new Boton("Atacar",atacarInicioEventHandler);
 
@@ -196,7 +205,7 @@ public class ContenedorPrincipal extends BorderPane {
     }
 
     public void dibujarMetodosCastillo(Castillo castillo) {
-        this.bottom = new VBox(); //Reinicio el vbox de bottom
+        this.bottom = new HBox(); //Reinicio el vbox de bottom
         BotonCrearArmaDeAsedioEventHandler armaDeAsedioEventHandler = new BotonCrearArmaDeAsedioEventHandler(juego,castillo,this);
         Boton crearArmaDeAsedio = new Boton("Crear arma de asedio",armaDeAsedioEventHandler);
         setjugadorActual();
