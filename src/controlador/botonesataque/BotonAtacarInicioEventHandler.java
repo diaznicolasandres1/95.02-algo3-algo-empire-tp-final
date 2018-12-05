b7862df09bf1b29eecd0cc039896f82b36a7675d
Facundo Mastricchio <facundomastricchio@gmail.com>
2018-12-04 23:26:56 -0300

    Elimino parametros innecesarios

--- src/controlador/botonesataque/BotonAtacarInicioEventHandler.java
+++ src/controlador/botonesataque/BotonAtacarInicioEventHandler.java
@@ -3,17 +3,17 @@ package controlador.botonesataque;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
-import modelo.juego.Juego;
+import javafx.scene.layout.Region;
 import modelo.unidades.Atacante;
 import vista.ContenedorPrincipal;
 
 public class BotonAtacarInicioEventHandler implements EventHandler<ActionEvent> {
+
     private Atacante atacante;
-    private Juego juego;
     private ContenedorPrincipal contenedorPrincipal;
-    public BotonAtacarInicioEventHandler(Juego juego, Atacante atacante, ContenedorPrincipal contenedorPrincipal){
+
+    public BotonAtacarInicioEventHandler(Atacante atacante, ContenedorPrincipal contenedorPrincipal) {
         this.atacante = atacante;
-        this.juego = juego;
         this.contenedorPrincipal = contenedorPrincipal;
     }
 
@@ -22,6 +22,7 @@ public class BotonAtacarInicioEventHandler implements EventHandler<ActionEvent> 
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("");
         alert.setContentText("Haz click en el sobre quien quieres realizar el ataque");
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
         alert.show();
         this.contenedorPrincipal.cambiarHandlerAtaque(this.atacante);
 
